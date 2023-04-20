7ca2b7b2b7c81fe3250d19624334122e87bb834b
kos <ustyugovki@bk.ru>
2023-04-20 12:25:01 +0300

    Изменил Form. Добавил Scroll To

--- source/js/modules/form-validate/callback.js
+++ source/js/modules/form-validate/callback.js
@@ -1,12 +1,12 @@
 
 const baseSuccessCallback = (event) => {
-  event.preventDefault();
+  // event.preventDefault();
+  event.target.submit();
   // В данном колбеке бэкендер, либо разработчик при необходимости будет писать запрос на отправку формы на сервер и обрабатывать возможные ошибки или успешную отправку формы на сервер
 };
 
 const baseErrorCallback = (event) => {
-  // event.preventDefault();
-  event.target.submit();
+  event.preventDefault();
   // Данный коллбек используется при необходимости выполнить какое-либо действие помимо показа ошибок при попытке отправить неккорректные данные, он не связан с запросами на сервер
 };
 
